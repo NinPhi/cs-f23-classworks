d0431a39770b4ec28d2ba61397b4180cc219018c
NinPhi <melmarka4@gmail.com>
2023-12-22 20:45:42 +0600

    L3 Exception Handling

---
+++ L3_ExceptionHandling/L3_ExceptionHandling/Exceptions/CannotDivideByThreeException.cs
@@ -0,0 +1,8 @@
+﻿namespace L3_ExceptionHandling.Exceptions;
+
+internal class CannotDivideByThreeException : Exception
+{
+    public CannotDivideByThreeException() : base(
+        "Запрещено делить на три!")
+    { }
+}
---
+++ L3_ExceptionHandling/L3_ExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using L3_ExceptionHandling.Exceptions;
+
+while (true)
+{
+    try
+    {
+        Start();
+    }
+    catch (DivideByZeroException)
+    {
+        Console.Clear();
+        Console.WriteLine("Невозможно поделить число на ноль.");
+    }
+    catch (Exception ex)
+    {
+        Console.Clear();
+        Console.WriteLine("Что-то пошло не так:");
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+void Start()
+{
+    Console.WriteLine("====================");
+    Console.WriteLine("Enter a number: ");
+
+    int input = int.Parse(Console.ReadLine() ?? string.Empty);
+
+    Console.WriteLine($"You entered {input}!");
+
+    if (input == 3)
+        throw new CannotDivideByThreeException();
+
+    int result = 100 / input;
+
+    Console.WriteLine($"100 divided by {input} = {result}");
+}
