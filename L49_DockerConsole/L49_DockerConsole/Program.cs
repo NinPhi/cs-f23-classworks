9d2fd6913793297a7775dfe00e3267ac490d6e0f
NinPhi <melmarka4@gmail.com>
2024-05-08 20:54:41 +0600

    L49

---
+++ L49_DockerConsole/L49_DockerConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿if (args == null || args.Length < 1)
+{
+    Console.WriteLine("Required arguments missing.");
+    Environment.Exit(0);
+}
+
+var arg1 = args[0];
+
+Console.WriteLine("Hello! Enter your name:");
+var name = "Azim";
+
+if (name == "Azim")
+    Console.WriteLine("Welcome back!");
+else
+{
+    Console.WriteLine($"Sorry, {name}... I don't talk to strangers.");
+    Environment.Exit(0);
+}
+
+Console.WriteLine("Here is your argument:");
+Console.WriteLine(arg1);
+
+while (true)
+{
+    Console.WriteLine("Hmmm...");
+    Thread.Sleep(500);
+}
